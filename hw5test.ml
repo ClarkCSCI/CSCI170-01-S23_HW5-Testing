2c1aeac0f8771a85c3d63254226110c467122b99
liamwangclarku <120049323+liamwangclarku@users.noreply.github.com>
2023-03-23 19:17:19 -0400

    Update hw5test.ml

--- hw5test.ml
+++ hw5test.ml
@@ -38,14 +38,38 @@ let dequePlusMinus = TwoListDeque.empty
 |> TwoListDeque.add_last 1
 |> TwoListDeque.remove_last
 |> TwoListDeque.add_first 1
-|> TwoListDeque.remove_first
-|> TwoListDeque.add_first 1
+|> TwoListDeque.remove_last
+|> TwoListDeque.add_last 1
 |> TwoListDeque.remove_first
 |> TwoListDeque.add_last 1
 |> TwoListDeque.remove_last
 ;;
 
 (** peek testing suite *)
+let test_peek_first_deque123 = (1 = TwoListDeque.peek_first deque123) in
+assert(test_peek_first_deque123 = true)
+;;
+let test_peek_last_deque123 = (3 = TwoListDeque.peek_last deque123) in
+assert(test_peek_last_deque123 = true)
+;;
+let test_peek_first_deque321 = (3 = TwoListDeque.peek_first deque321) in
+assert(test_peek_first_deque321 = true)
+;;
+let test_peek_last_deque321 = (1 = TwoListDeque.peek_last deque321) in
+assert(test_peek_last_deque321 = true)
+;;
+let test_peek_first_deque312 = (3 = TwoListDeque.peek_first deque312) in
+assert(test_peek_first_deque312 = true)
+;;
+let test_peek_last_deque312 = (2 = TwoListDeque.peek_last deque312) in
+assert(test_peek_last_deque312 = true)
+;;
+let test_peek_first_dequeMammaMia = (["Mamma"; "Mia"; "Here"; "We"; "Go"; "Again"] = TwoListDeque.peek_first dequeMammaMia) in
+assert(test_peek_first_dequeMammaMia = true)
+;;
+let test_peek_last_dequeMammaMia = (["Mamma"; "Mia"; "Here"; "We"; "Go"; "Again"] = TwoListDeque.peek_last dequeMammaMia) in
+assert(test_peek_last_dequeMammaMia = true)
+;;
 
 (** size testing suite *)
 let test_size_empty = (0 = TwoListDeque.size empty_deque) in
@@ -65,6 +89,12 @@ assert (test_size_dequecab = true)
 ;;
 let test_size_dequeABBA = (4 = TwoListDeque.size dequeABBA) in
 assert (test_size_dequeABBA = true)
+;;
+let test_size_dequePlusMinus = (0 = TwoListDeque.size dequePlusMinus) in
+assert (test_size_dequePlusMinus = true)
+;;
+let test_size_dequeMammaMia = (1 = TwoListDeque.size dequeMammaMia) in
+assert (test_size_dequeMammaMia = true)
 ;;
 
 (** deque to_list testing suite *)
@@ -87,6 +117,22 @@ let test_list_dequeABBA = (["A"; "B"; "B"; "A"] = TwoListDeque.to_list dequeABBA
 assert (test_list_dequeABBA = true)
 ;;
 
+(** testing for filter map and fold *)
+let test_filter_deque123 = ([2] = (TwoListDeque.filter (fun i -> i = 2) deque123 |> TwoListDeque.to_list)) in
+assert (test_filter_deque123 = true)
+;;
+let test_filter_dequecab = (["c"; "b"] = (TwoListDeque.filter (fun i -> i != "a") dequecab |> TwoListDeque.to_list)) in
+assert (test_filter_dequecab = true)
+;;
+let test_map_deque123 = ([2; 4; 6] = (TwoListDeque.map (fun i -> i * 2) deque123 |> TwoListDeque.to_list)) in
+assert (test_map_deque123 = true)
+;;
+let test_fold_first_dequecab = ("cab" = (TwoListDeque.fold_from_first_forward (^) "" dequecab)) in
+assert (test_fold_first_dequecab = true)
+;;
+let test_fold_last_dequecab = ("cab" = (TwoListDeque.fold_from_last_backward (^) dequecab "")) in
+assert (test_fold_last_dequecab = true)
+;;
 (* ! lev's tests ! *)
 (* ? testing empty/is_empty *)
 let test = TwoListDeque.empty
